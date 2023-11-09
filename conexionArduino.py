b93f68de57c192c3e5d49c7b5b177702120f2712
Diego Lechuga <diegolechuga2003@gmail.comgit config --list>
2023-11-08 22:37:15 -0600

    Materiales de clase

---
+++ conexionArduino.py
@@ -0,0 +1,23 @@
+import os
+import serial
+import time
+
+def arduino_read_data(file):
+    encoding = "utf-8"
+    #Iniciando la conexion serial
+    arduinoPort = serial.Serial('COM6',9600,timeout=1) #Cambiar la entrada del arduino en Windows
+    #arduinoPort = serial.Serial('/dev/ttyACM0',9600,timeout=1)  #Cambiar la entrada del arduino en linux
+    f = open(file,"w") #Abre el archivo dependiendo del valor que se manda, nombre el archivo txt
+
+    #Retardo para establecer la conexion serial
+    time.sleep(.3)  #Retardo para leer los datos, preferentemente debe ser igual al delay del código del arduino
+    for i in range (25):
+        getSerialValue = arduinoPort.readline()
+        print(getSerialValue)
+        f.write(getSerialValue.decode(encoding))
+
+    #Cerrando el puerto serial
+    arduinoPort.close()
+    f.close()
+
+arduino_read_data("lecturas.txt")
---
+++ perceptronNEntradas.py
@@ -0,0 +1,27 @@
+import numpy as np
+escalon = lambda x: 0 if x < 0 else 1
+p=np.array([[1,1,1,1,1,1,0],
+            [0,1,1,0,0,0,0],
+            [1,1,0,1,1,0,1],
+            [1,1,1,1,0,0,1],
+            [0,1,1,0,0,1,1],
+            [1,0,1,1,0,1,1],
+            [1,0,1,1,1,1,1],
+            [1,1,1,0,0,0,0],
+            [1,1,1,1,1,1,1],
+            [1,1,1,0,0,1,1]])
+p=p.T
+
+tpar=[1,0,1,0,1,0,1,0,1,0]
+t=tpar
+w=2*np.random.rand(1,7)-1
+b=2*np.random.rand(1)-1
+e=np.zeros(10)
+for epocas in range(500):
+    for q in range(len(p)):    
+        e[q]=t[q]-escalon(np.dot(w,p[:,q])+b)
+        w=w+(e[q]*(p[:,q]).T)
+        b=b+e[q]
+print("Error:",e)
+print("Pesos:",w)
+print("Polarizacion:",b)
